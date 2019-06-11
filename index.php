80471aedbcd1750581664861c279ff10830245fb
muh riansyah <mriansyah93@gmail.com>
2019-06-11 09:34:31 +0700

    iter 2

--- index.php
+++ index.php
@@ -37,23 +37,23 @@
 			<!-- Main Navigation -->
 			<nav class="main_nav">
 				<ul class="d-flex flex-row align-items-center justify-content-start">
-					<li class="active"><a href="index.html">Home</a></li>
-					<li><a href="about.html">About us</a></li>
-					<li><a href="services.html">Services</a></li>
-					<li><a href="blog.html">News</a></li>
-					<li><a href="contact.html">Contact</a></li>
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="#">Tentang Kami</a></li>
+					<li><a href="#pelayanan">Pelayanan</a></li>
+					<li><a href="#">Berita</a></li>
+					<li><a href="#">Kontak</a></li>
 				</ul>
 			</nav>
 			<div class="header_extra d-flex flex-row align-items-center justify-content-end ml-auto">
 				
 				<!-- Work Hourse -->
-				<div class="work_hours">Mo - Sat: 8:00am - 9:00pm</div>
+				<div class="work_hours">Senin - Minggu: 8:00  - 22:00 </div>
 
 				<!-- Header Phone -->
-				<div class="header_phone">+34 586 778 8892</div>
+				<div class="header_phone">(021) 8269 3888</div>
 
 				<!-- Appointment Button -->
-				<div class="button button_1 header_button"><a href="#">Make an Appointment</a></div>
+				<div class="button button_1 header_button"><a href="#">Buat Janji</a></div>
 
 				<!-- Header Social -->
 				<div class="social header_social">
@@ -77,17 +77,17 @@
 		<div class="menu_close_container"><div class="menu_close"><div></div><div></div></div></div>
 		<nav class="menu_nav">
 			<ul>
-				<li><a href="index.html">Home</a></li>
-				<li><a href="about.html">About us</a></li>
-				<li><a href="services.html">Services</a></li>
-				<li><a href="blog.html">News</a></li>
-				<li><a href="contact.html">Contact</a></li>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About us</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">News</a></li>
+				<li><a href="#">Contact</a></li>
 			</ul>
 		</nav>
 		<div class="menu_extra">
 			<div class="menu_link">Mo - Sat: 8:00am - 9:00pm</div>
-			<div class="menu_link">+34 586 778 8892</div>
-			<div class="menu_link"><a href="#">Make an appointment</a></div>
+			<div class="menu_link">(021) 8269 3888</div>
+			<div class="menu_link"><a href="#">Buat Janji</a></div>
 		</div>
 		<div class="social menu_social">
 			<ul class="d-flex flex-row align-items-center justify-content-start">
@@ -107,77 +107,38 @@
 			<div class="owl-carousel owl-theme home_slider">
 				
 				<!-- Slide -->
-				<div class="owl-item">
-					<div class="background_image" style="background-image:url(images/home_slider.jpg)"></div>
-					<div class="home_container">
-						<div class="container">
-							<div class="row">
-								<div class="col">
-									<div class="home_content">
-										<div class="home_subtitle">#1 Klinik Terbaik </div>
-										<div class="home_title">Kesehatan anda prioritas kami</div>
-										<div class="home_text">
-											<p>Layanan Fasilitas Kesehatan komprehensif, dengan sajian pelayanan yang Edukatif, Preventif dan Kuratif untuk Generasi Penerus Bangsa yang Gemilang</p>
-										</div>
-										<div class="home_buttons d-flex flex-row align-items-center justify-content-start">
-											<div class="button button_1 trans_200"><a href="#">read more</a></div>
-											<div class="button button_1 trans_200"><a href="#">make an appointment</a></div>
+				<?php 
+				$slides = array(
+					'home_slider.jpg'=> '',
+					'front_office.jpeg'=> '',
+					'blog_1.jpg'=> ''
+				);
+
+				foreach ($slides as $key => $value) {
+					
+					?>
+					<div class="owl-item">
+						<div class="background_image" style="background-image:url(images/<?php echo $key ?>)"></div>
+						<div class="home_container">
+							<div class="container">
+								<div class="row">
+									<div class="col">
+										<div class="home_content">
+											<div class="home_subtitle">#1 Klinik Terbaik </div>
+											<div class="home_title">Kesehatan anda prioritas kami</div>
+											<div class="home_text">
+												<p>Layanan Fasilitas Kesehatan komprehensif, dengan sajian pelayanan yang Edukatif, Preventif dan Kuratif untuk Generasi Penerus Bangsa yang Gemilang</p>
+											</div>
+											<div class="home_buttons d-flex flex-row align-items-center justify-content-start">
+												<div class="button button_1 trans_200"><a href="#">Buat Janji</a></div>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-
-				<!-- Slide -->
-				<div class="owl-item">
-					<div class="background_image" style="background-image:url(images/home_slider.jpg)"></div>
-					<div class="home_container">
-						<div class="container">
-							<div class="row">
-								<div class="col">
-									<div class="home_content">
-										<div class="home_subtitle">#1 Plastic Surgery Clinic</div>
-										<div class="home_title">Love the new you</div>
-										<div class="home_text">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p>
-										</div>
-										<div class="home_buttons d-flex flex-row align-items-center justify-content-start">
-											<div class="button button_1 trans_200"><a href="#">read more</a></div>
-											<div class="button button_1 trans_200"><a href="#">make an appointment</a></div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Slide -->
-				<div class="owl-item">
-					<div class="background_image" style="background-image:url(images/home_slider.jpg)"></div>
-					<div class="home_container">
-						<div class="container">
-							<div class="row">
-								<div class="col">
-									<div class="home_content">
-										<div class="home_subtitle">#1 Plastic Surgery Clinic</div>
-										<div class="home_title">Love the new you</div>
-										<div class="home_text">
-											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p>
-										</div>
-										<div class="home_buttons d-flex flex-row align-items-center justify-content-start">
-											<div class="button button_1 trans_200"><a href="#">read more</a></div>
-											<div class="button button_1 trans_200"><a href="#">make an appointment</a></div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
+			<?php } ?>
 			</div>
 
 			<!-- Home Slider Dots -->
@@ -203,8 +164,7 @@
 				<div class="col-lg-6 intro_col">
 					<div class="intro_content">
 						<div class="section_title_container">
-							<div class="section_subtitle">This is Dr Pro</div>
-							<div class="section_title"><h2>Welcome to our Clinic</h2></div>
+							<div class="section_title"><h2>Selamat Datang Ke Klinik Kami</h2></div>
 						</div>
 						<div class="intro_text">
 							<p>Integer aliquet congue libero, eu gravida odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl. Duis feugiat bibendum orci, non elementum urna vestibulum in. Nulla facilisi. Nulla egestas vel lacus sed interdum. Sed mollis, orci elementum eleifend tempor, nunc libero porttitor tellus, vel pharetra metus dolor.</p>
@@ -244,7 +204,7 @@
 				<!-- Intro Form -->
 				<div class="col-lg-6 intro_col">
 					<div class="intro_form_container">
-						<div class="intro_form_title">Make an Appointment</div>
+						<div class="intro_form_title">Buat Janji</div>
 						<form action="#" class="intro_form" id="intro_form">
 							<div class="d-flex flex-row align-items-start justify-content-between flex-wrap">
 								<input type="text" class="intro_input" placeholder="Your Name" required="required">
@@ -268,7 +228,7 @@
 									</select>
 								<input type="text" id="datepicker" class="intro_input datepicker" placeholder="Date" required="required">
 							</div>
-							<button class="button button_1 intro_button trans_200">make an appointment</button>
+							<button class="button button_1 intro_button trans_200">Buat Janji</button>
 						</form>
 					</div>
 				</div>
@@ -280,7 +240,7 @@
 	<!-- Why Choose Us -->
 
 	<div class="why">
-		<!-- <div class="background_image" style="background-image:url(images/why.jpg)"></div> -->
+		<div class="background_image" style="background-image:url(images/why.jpg)"></div>
 		<div class="container">
 			<div class="row row-eq-height">
 
@@ -295,11 +255,10 @@
 				<div class="col-lg-6 order-lg-2 order-1">
 					<div class="why_content">
 						<div class="section_title_container">
-							<div class="section_subtitle">This is Dr Pro</div>
-							<div class="section_title"><h2>Why choose us?</h2></div>
+							<div class="section_title"><h2>Kenapa memilih kami?</h2></div>
 						</div>
 						<div class="why_text">
-							<p>Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl. Duis feugiat bibendum orci, non elementum urna vestibulum in. Nulla facilisi. Nulla egestas vel lacus sed interdum. Sed mollis, orci elementum eleifend tempor, nunc libero porttitor tellus, vel pharetra metus dolor.</p>
+							<p>Ada begitu banyak klinik tapi cuman klinik puri intan blabla . Duis feugiat bibendum orci, non elementum urna vestibulum in. Nulla facilisi. Nulla egestas vel lacus sed interdum. Sed mollis, orci elementum eleifend tempor, nunc libero porttitor tellus, vel pharetra metus dolor.</p>
 						</div>
 						<div class="why_list">
 							<ul>
@@ -310,7 +269,7 @@
 										<div class="icon"><img src="images/icon_1.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
 									</div>
 									<div class="why_list_content">
-										<div class="why_list_title">Only Top Products</div>
+										<div class="why_list_title">Obat Terbaik</div>
 										<div class="why_list_text">Etiam ac erat ut enim maximus accumsan vel ac nisl</div>
 									</div>
 								</li>
@@ -321,7 +280,7 @@
 										<div class="icon"><img src="images/icon_2.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
 									</div>
 									<div class="why_list_content">
-										<div class="why_list_title">The best Doctors</div>
+										<div class="why_list_title">Dokter Terbaik di bidangnya</div>
 										<div class="why_list_text">Ac erat ut enim maximus accumsan vel ac</div>
 									</div>
 								</li>
@@ -332,7 +291,7 @@
 										<div class="icon"><img src="images/icon_3.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
 									</div>
 									<div class="why_list_content">
-										<div class="why_list_title">Great Feedback</div>
+										<div class="why_list_title">Respon cepat</div>
 										<div class="why_list_text">Etiam ac erat ut enim maximus accumsan vel</div>
 									</div>
 								</li>
@@ -353,10 +312,10 @@
 				<div class="col">
 					<div class="cta_container d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-start">
 						<div class="cta_content">
-							<div class="cta_title">Make your appointment today!</div>
+							<div class="cta_title">Segera buat janji hari ini!</div>
 							<div class="cta_text">Etiam ac erat ut enim maximus accumsan vel ac nisl</div>
 						</div>
-						<div class="cta_phone ml-lg-auto">+34 586 778 8892</div>
+						<div class="cta_phone ml-lg-auto">(021) 8269 3888</div>
 					</div>
 				</div>
 			</div>
@@ -365,7 +324,7 @@
 
 	<!-- Services -->
 
-	<div class="services">
+	<div id="pelayanan" class="services">
 		<div class="container">
 			<div class="row">
 				<div class="col text-center">
@@ -374,7 +333,17 @@
 						<div class="section_title"><h2>Pelayanan Kami</h2></div>
 						<?php 
 
-						$arr = ['Umum', 'Kebidanan', 'Poli Gigi', 'Poli Laktasi', 'Pijat Bayi', 'Senam Hamil', 'Home Care', 'Poli Tumbuh Kembang', 'Farmasi'] 
+						$services = array(
+							'Umum'=> 'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl',
+							'Kebidanan'=>'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl',
+							'Poli Gigi'=>'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl',
+							'Poli Laktasi'=>'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl',
+							'Pijat Bayi'=>'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl',
+							'Senam Hamil'=>'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl',
+							'Home Care'=>'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl',
+							'Poli Tumbuh Kembang'=>'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl',
+							'Farmasi'=> 'Poli umum kami siap melayani keluhan Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl'
+						);
 						?>
 					</div>
 				</div>
@@ -382,94 +351,22 @@
 			<div class="row services_row">
 				
 				<!-- Service -->
-				<div class="col-xl-4 col-md-6 service_col">
-					<div class="service text-center">
-						<div class="service">
-							<div class="icon_container d-flex flex-column align-items-center justify-content-center ml-auto mr-auto">
-								<div class="icon"><img src="images/icon_4.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
-							</div>
-							<div class="service_title">Umum, Kebidanan, Poli Gigi, Poli Laktasi, Pijat Bayi, Senam Hamil, Home Care, Poli Tumbuh Kembang, Farmasi.</div>
-							<div class="service_text">
-								<p>Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl. Duis feugiat bibendum orci, non elementum urna.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Service -->
-				<div class="col-xl-4 col-md-6 service_col">
-					<div class="service text-center">
-						<div class="service">
-							<div class="icon_container d-flex flex-column align-items-center justify-content-center ml-auto mr-auto">
-								<div class="icon"><img src="images/icon_5.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
-							</div>
-							<div class="service_title">Breast Augmentation</div>
-							<div class="service_text">
-								<p>Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl. Duis feugiat bibendum orci, non elementum urna.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Service -->
-				<div class="col-xl-4 col-md-6 service_col">
-					<div class="service text-center">
-						<div class="service">
-							<div class="icon_container d-flex flex-column align-items-center justify-content-center ml-auto mr-auto">
-								<div class="icon"><img src="images/icon_6.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
-							</div>
-							<div class="service_title">Breast Augmentation</div>
-							<div class="service_text">
-								<p>Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl. Duis feugiat bibendum orci, non elementum urna.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Service -->
-				<div class="col-xl-4 col-md-6 service_col">
-					<div class="service text-center">
-						<div class="service">
-							<div class="icon_container d-flex flex-column align-items-center justify-content-center ml-auto mr-auto">
-								<div class="icon"><img src="images/icon_7.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
-							</div>
-							<div class="service_title">Breast Augmentation</div>
-							<div class="service_text">
-								<p>Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl. Duis feugiat bibendum orci, non elementum urna.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Service -->
-				<div class="col-xl-4 col-md-6 service_col">
-					<div class="service text-center">
-						<div class="service">
-							<div class="icon_container d-flex flex-column align-items-center justify-content-center ml-auto mr-auto">
-								<div class="icon"><img src="images/icon_8.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
-							</div>
-							<div class="service_title">Breast Augmentation</div>
-							<div class="service_text">
-								<p>Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl. Duis feugiat bibendum orci, non elementum urna.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<!-- Service -->
+				<?php foreach ($services as $key => $value) { ?>
 				<div class="col-xl-4 col-md-6 service_col">
 					<div class="service text-center">
 						<div class="service">
 							<div class="icon_container d-flex flex-column align-items-center justify-content-center ml-auto mr-auto">
 								<div class="icon"><img src="images/icon_3.svg" alt="https://www.flaticon.com/authors/prosymbols"></div>
 							</div>
-							<div class="service_title">Breast Augmentation</div>
+							<div class="service_title"><?php echo $key ?></div>
 							<div class="service_text">
-								<p>Odio ultrices ut. Etiam ac erat ut enim maximus accumsan vel ac nisl. Duis feugiat bibendum orci, non elementum urna.</p>
+								<p><?php echo $value ?></p>
 							</div>
 						</div>
 					</div>
-				</div>
+				</div>	
+				<?php } ?>
+				
 
 			</div>
 		</div>
@@ -617,11 +514,11 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 </div>
 							<nav class="footer_nav ml-md-auto">
 								<ul class="d-flex flex-row align-items-center justify-content-start">
-									<li><a href="index.html">Home</a></li>
-									<li><a href="about.html">About us</a></li>
-									<li><a href="services.html">Services</a></li>
-									<li><a href="blog.html">News</a></li>
-									<li><a href="contact.html">Contact</a></li>
+									<li><a href="#">Home</a></li>
+									<li><a href="#">About us</a></li>
+									<li><a href="#">Services</a></li>
+									<li><a href="#">News</a></li>
+									<li><a href="#">Contact</a></li>
 								</ul>
 							</nav>
 						</div>
